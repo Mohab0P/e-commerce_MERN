eadbc7a838a6a4295f8b537f118907f82bc6306a
Mohab0P <mohabop@icloud.com>
2024-11-02 19:25:47 +0300

    "We are back" add  simple navbar using Tailwind CSS

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,12 @@
 import { BrowserRouter,Route,Routes } from "react-router-dom";
 import  HomePage  from "./pages/HomePage";
+import { NavBar } from "./components/NavBar";
+import './index.css';
 
 function App() {
   return(
     <BrowserRouter>
+    <NavBar/>
     <Routes>
       <Route path="/" element={<HomePage/>}/>
       
